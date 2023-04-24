84bb0dbad41381d45cd566be677be086a1a9f3f2
Alexey Erokhin <meffulka@trafficstars.com>
2023-04-24 21:49:47 +0300

    add remove roles

--- bot.py
+++ bot.py
@@ -33,8 +33,12 @@ async def on_message(message):
         god_name, token = message.content.split(':')
         hero, status = godvile.get_hero(god_name=god_name,token=token)
         if status==200 and hero['clan'] == "Хренелли":
+            member = server.get_member(message.author.id)
+            member_roles = [role.name for role in member.roles if role.name == "Отступник" or role.name == "Семья" or role.name == "Соучастник"]
+
             await log_channel.send(f'``{message.author}`` привязал бога ``{god_name}``')
             await message.author.send("Привязал, спасибо!")
+
             if is_good_god(hero['alignment']):
                 role = discord.utils.get(server.roles, name = "Отступник")
             else:
@@ -42,13 +46,19 @@ async def on_message(message):
                     role = discord.utils.get(server.roles, name = "Семья")
                 else:
                     role = discord.utils.get(server.roles, name = "Соучастник")
-            member = server.get_member(message.author.id)
+
             await member.add_roles(role)
             await log_channel.send(f'``{message.author}`` выдана роль ``{role.name}``')
+
+            for r in member_roles.remove(role.name):
+                remove_role = discord.utils.get(server.roles, name = r)
+                member.remove_roles(remove_role)
+                await log_channel.send(f'``{message.author}`` удалена роль ``{role.name}``')
+
             if member.id != server.owner_id:
                 try:
                     await member.edit(nick=god_name)
-                    await log_channel.send(f'``{message.author}`` поменян ник на  {god_name}')
+                    await log_channel.send(f'``{message.author}`` поменян ник на  ``{god_name}``')
                 except discord.Forbidden:
                     await log_channel.send(f'Не хватило прав на изменения ника для ``{message.author}``')
         else:
